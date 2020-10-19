076c84897e9a04c4ff2d6b856d6c55174f96ed9c
dougfane <douglas.chagas02@outlook.com>
2020-10-19 11:40:36 -0300

    Broken flex

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,27 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-10.9684256,-37.0833271], 16);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+//create and add marker
+L.marker([-10.9684256,-37.0833271], {icon})
+    .addTo(map)
