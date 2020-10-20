6a04b0c78ca73b646559f53b27deb9808e9b1d67
dougfane <douglas.chagas02@outlook.com>
2020-10-20 10:07:11 -0300

    Image gallery complete

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -31,5 +31,8 @@ function selectImage(event) {
     const button = event.currentTarget
     const buttons = document.querySelectorAll(".images button")
     buttons.forEach(function (button) {button.classList.remove("active")})
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+    imageContainer.src = image.src
     button.classList.add("active")
 } 
